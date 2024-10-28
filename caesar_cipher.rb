6793f31e6460420ff128a14d15dd3a09c45e5d00
cj <90126284+CeeJank@users.noreply.github.com>
2024-10-28 11:07:05 +0800

    finished

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,11 +1,12 @@
-alphabet_array = ('a'..'z').to_a
 
-
-def caesar_cipher(string, shift)
-  string_arr = string.split("")
-  p string_arr
-
+def caesar_cipher(string, shift = 1)
+  alpha = Array('a'..'z') #creates an array of all alphabets
+  encrypter  = Hash[alpha.zip(alpha.rotate(shift))] #converts arguments to arrays, then rotates both elements 
+  p encrypter
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+  
 end  
 
+p caesar_cipher("testing", 3).join
 
 
