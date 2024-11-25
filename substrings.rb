1ff179be9e33f5a950bd86b617de2d1652047e5e
cj <90126284+CeeJank@users.noreply.github.com>
2024-11-25 23:15:36 +0800

    finished

---
+++ substrings.rb
@@ -0,0 +1,27 @@
+# Convert word to array
+# loop through dictionary for each word and determine if hash has key
+# key exists, iterate value, if not add value 1
+
+
+def substrings(word, dictionary)
+   hash = {} # empty hash to store output
+   string_array = word.downcase.split 
+   string_array.each do |i| # nested loop to iterate through dictionary for each element
+      dictionary.each do |j|
+         if i.include?(j)
+            if hash.has_key?(j)
+               hash[j] += 1
+            else hash[j] = 1
+            end
+         end
+      end
+   end
+   puts hash
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
